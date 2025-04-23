5de834043d28dd1c73b3fdc2deb2c4b021ef02bc
gyesswhat <h2weirdo@icloud.com>
2025-04-23 15:16:46 +0900

    fix: winning entry 하드코딩에서 db로 전환

--- src/main/java/com/example/__unis_fest_back/service/AdminService.java
+++ src/main/java/com/example/__unis_fest_back/service/AdminService.java
@@ -2,7 +2,9 @@ package com.example.__unis_fest_back.service;
 
 import com.example.__unis_fest_back.dto.DrawInfoDto;
 import com.example.__unis_fest_back.entity.User;
+import com.example.__unis_fest_back.entity.WinningEntry;
 import com.example.__unis_fest_back.repository.UserRepository;
+import com.example.__unis_fest_back.repository.WinningEntryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +14,18 @@ import java.util.ArrayList;
 @RequiredArgsConstructor
 public class AdminService {
     private final UserRepository userRepository;
+    private final WinningEntryRepository winningEntryRepository;
 
     public ArrayList<DrawInfoDto> getDrawInfo() {
         ArrayList<User> users = userRepository.findByEntryTimeNotNullOrderByEntryTimeDesc();
         ArrayList<DrawInfoDto> responses = new ArrayList<>();
         for (User user : users) {
+            WinningEntry winningEntry = winningEntryRepository.findByUser_OrderNumber(user.getOrderNumber());
             responses.add(new DrawInfoDto(
                     user.getOrderNumber(),
                     user.getEntryTime(),
                     user.getPhoneNumber(),
-                    DrawService.WINNING_PRIZES.get(user.getOrderNumber())
+                    winningEntry.getPrizeName()
             ));
         }
         return responses;
