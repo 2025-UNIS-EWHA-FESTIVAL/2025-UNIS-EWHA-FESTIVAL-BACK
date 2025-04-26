b62472a118f86f703638771c29fa6c497909063d
gyesswhat <h2weirdo@icloud.com>
2025-04-27 03:47:28 +0900

    fix: LoginController 추가

---
+++ src/main/java/com/example/__unis_fest_back/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.example.__unis_fest_back.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+}
