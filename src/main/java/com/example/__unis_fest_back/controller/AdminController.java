8008004dc9cb605cc3176bbba8588fc00b25e18d
gyesswhat <h2weirdo@icloud.com>
2025-04-27 03:21:26 +0900

    fix: 로그인 페이지 에러 수정

--- src/main/java/com/example/__unis_fest_back/controller/AdminController.java
+++ src/main/java/com/example/__unis_fest_back/controller/AdminController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
