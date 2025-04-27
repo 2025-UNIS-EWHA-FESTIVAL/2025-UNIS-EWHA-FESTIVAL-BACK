3f60b0dc7f611b384d224929df1bada1dd0245b5
gyesswhat <h2weirdo@icloud.com>
2025-04-27 11:41:39 +0900

    chore: 헤더 로그 찍기

--- src/main/java/com/example/__unis_fest_back/controller/HealthController.java
+++ src/main/java/com/example/__unis_fest_back/controller/HealthController.java
@@ -1,9 +1,13 @@
 package com.example.__unis_fest_back.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import jakarta.servlet.http.HttpServletRequest;
 
+
+@Slf4j
 @RestController
 public class HealthController {
 
@@ -11,4 +15,12 @@ public class HealthController {
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("OK");
     }
+
+    @GetMapping("/test")
+    public String index(HttpServletRequest request) {
+        String proto = request.getHeader("X-Forwarded-Proto");
+        log.info("🔵 X-Forwarded-Proto = " + proto);
+        return "ok";
+    }
+
 }
