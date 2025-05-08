695516ae30b19b74ec922435ddba0f60ce662601
gyesswhat <h2weirdo@icloud.com>
2025-05-09 02:17:57 +0900

    feat: 서버 운영 여부 체크 API 추가

---
+++ src/main/java/com/example/__unis_fest_back/controller/StatusController.java
@@ -0,0 +1,22 @@
+package com.example.__unis_fest_back.controller;
+
+import com.example.__unis_fest_back.global.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+
+@RestController
+public class StatusController {
+    @GetMapping("/api/status")
+    public ResponseEntity<ApiResponse<?>> checkServerStatus() {
+        LocalTime now = LocalTime.now();
+
+        if (now.isAfter(LocalTime.of(22, 59)) || now.isBefore(LocalTime.of(9, 0))) {
+            return ResponseEntity.ok(ApiResponse.success(false));
+        }
+        else return ResponseEntity.ok(ApiResponse.success(true));
+    }
+}
